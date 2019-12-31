ccfc8f02be3acedd07330e8ed6abe964c2334e96
Jason von Nieda <jason@vonnieda.org>
2019-12-30 22:25:33 -0600

    Converted some of the routes to async / await. Only one left is the post, but busboy uses events and I don't feel like diving into that mess right now.

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ const db = new Pool({
   ssl: true,
 });
 
-const sendgrid  = require('sendgrid')(config.sendgridUsername, config.sendgridPassword);
+const sendgrid = require('sendgrid')(config.sendgridUsername, config.sendgridPassword);
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -50,6 +50,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/pkgs', express.static(path.join(__dirname, '/bower_components')));
 
 /**
+ * Convert app.render to an async function for use in async routes.
+ * @param name
+ * @param options
+ * @returns {Promise<unknown>}
+ */
+async function render(name, options) {
+    return new Promise((resolve, reject) => {
+        app.render(name, options, function(err, res) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(res);
+            }
+        })
+    });
+}
+
+/**
  * Simple HTTP basic auth that looks for a valid, signed JWT in the password
  * field. Username is ignored.
  */
@@ -59,7 +78,7 @@ app.use(function (req, res, next) {
         return res.status(401).end();
     };
 
-    var user = basicAuth(req);
+    const user = basicAuth(req);
 
     if (!user || !user.name || !user.pass) {
         return unauthorized(res);
@@ -74,24 +93,29 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', async function(req, res, next) {
-    const entries = await getEntries();
-    _.each(entries, function(entry) {
-        entry.formattedDate = moment(entry.createdat).format('MMMM Do YYYY');
-        entry.formattedDay = moment(entry.createdat).format('dddd');
-        entry.formattedText = '<p>' + entry.text;
-        entry.formattedText = entry.formattedText.replace(/\r/g, '');
-        entry.formattedText = entry.formattedText.replace(/\n\n/g, '<p>');
-        entry.formattedText = entry.formattedText.replace(/\n/g, ' ');
-        entry.formattedText = entry.formattedText.trim();
-    });
-    res.render('index', {
-        name : config.name,
-        entries : entries
-    });
+    try {
+        const entries = await getEntries();
+        _.each(entries, function (entry) {
+            entry.formattedDate = moment(entry.createdat).format('MMMM Do YYYY');
+            entry.formattedDay = moment(entry.createdat).format('dddd');
+            entry.formattedText = '<p>' + entry.text;
+            entry.formattedText = entry.formattedText.replace(/\r/g, '');
+            entry.formattedText = entry.formattedText.replace(/\n\n/g, '<p>');
+            entry.formattedText = entry.formattedText.replace(/\n/g, ' ');
+            entry.formattedText = entry.formattedText.trim();
+        });
+        res.render('index', {
+            name: config.name,
+            entries: entries
+        });
+    }
+    catch (err) {
+        next(err);
+    }
 });
 
 app.post('/emails', function(req, res, next) {
-    var fields = {};
+    const fields = {};
     req.busboy.on('field', function(field, value) {
         fields[field] = value;
     });
@@ -99,7 +123,7 @@ app.post('/emails', function(req, res, next) {
         if (!fields.plain) {
             return res.status(404).end();
         }
-        var doc = {
+        const doc = {
             createdat : new Date(),
             text : email.extractEmailText(fields.plain)
         };
@@ -112,55 +136,41 @@ app.post('/emails', function(req, res, next) {
     });
 });
 
-app.post('/jobs/send', function(req, res, next) {
+app.post('/jobs/send', async function(req, res, next) {
     function createPreviousEntriesUrl(webRoot) {
-        var token = jwt.sign({},
+        const token = jwt.sign({},
             config.jwtSecret,
             { expiresIn : '24h' });
-        var url = URL.parse(webRoot);
+        const url = URL.parse(webRoot);
         url.auth = 'a:' + token;
         return URL.format(url);
     }
 
-    async.auto({
-        previousEntry : getRandomEntry,
-        subject : [function(callback, results) {
-            var date = moment().format('dddd, MMM Do');
-            var params = {
-                date : date
-            };
-            app.render('email-subject', params, callback);
-        }],
-        body : ['previousEntry', function(callback, results) {
-            var previousEntry = results.previousEntry;
-            var params = {
-                previousEntriesUrl : createPreviousEntriesUrl(config.webRoot)
-            };
-            if (previousEntry) {
-                params.previousEntryDate = capitalize(countdown(previousEntry.createdat, null, null, 2));
-                params.previousEntryBody = previousEntry.text;
-            }
-            app.render('email-body-text', params, callback);
-        }],
-        bodyHtml : ['previousEntry', function(callback, results) {
-            var previousEntry = results.previousEntry;
-            var params = {
-                previousEntriesUrl : createPreviousEntriesUrl(config.webRoot)
-            };
-            if (previousEntry) {
-                params.previousEntryDate = capitalize(countdown(previousEntry.createdat, null, null, 2));
-                params.previousEntryBody = previousEntry.text.replace(/\n/g, '<br>');
-            }
-            app.render('email-body-html', params, callback);
-        }]
-    }, function(err, results) {
-        if (err) {
-            return next(err);
-        }
+    try {
+        const previousEntry = await getRandomEntry();
+        const previousEntriesUrl = createPreviousEntriesUrl(config.webRoot);
 
-        var subject = results.subject;
-        var body = results.body;
-        var bodyHtml = results.bodyHtml;
+        const subject = await render('email-subject', {
+            date : moment().format('dddd, MMM Do'),
+        });
+
+        let bodyParams = {
+            previousEntriesUrl : createPreviousEntriesUrl(config.webRoot),
+        };
+        if (previousEntry) {
+            bodyParams.previousEntryDate = capitalize(countdown(previousEntry.createdat, null, null, 2));
+            bodyParams.previousEntryBody = previousEntry.text;
+        }
+        const body = await render('email-body-text', bodyParams);
+
+        let bodyHtmlParams = {
+            previousEntriesUrl : createPreviousEntriesUrl(config.webRoot),
+        };
+        if (previousEntry) {
+            bodyHtmlParams.previousEntryDate = capitalize(countdown(previousEntry.createdat, null, null, 2));
+            bodyHtmlParams.previousEntryBody = previousEntry.text.replace(/\n/g, '<br>');
+        }
+        const bodyHtml = await render('email-body-html', bodyHtmlParams);
 
         sendgrid.send({
             to: config.email,
@@ -176,23 +186,26 @@ app.post('/jobs/send', function(req, res, next) {
             }
             res.status(200).end();
         });
-    });
+    }
+    catch (err) {
+        return next(err);
+    }
 });
 
 async function getRandomEntry() {
-    var { rows } = await db.query(`
+    let rows = (await db.query(`
         select * from entries 
         where date_part('month', createdat) = date_part('month', now()) 
             and date_part('day', createdat) = date_part('day', now())
-    `);
+    `)).rows;
     if (!rows.length) {
-        var { rows } = await db.query("select * from entries where date_part('dow', createdat) = date_part('dow', now())");
+        rows = (await db.query("select * from entries where date_part('dow', createdat) = date_part('dow', now())")).rows;
     }
     if (!rows.length) {
-        var { rows } = await db.query("select * from entries where date_part('day', createdat) = date_part('day', now())");
+        rows = (await db.query("select * from entries where date_part('day', createdat) = date_part('day', now())")).rows;
     }
     if (!rows.length) {
-        var { rows } = await db.query("select * from entries");
+        rows = (await db.query("select * from entries"));
     }
     return _.sample(rows);
 }
@@ -221,8 +234,8 @@ function capitalize(str) {
 module.exports = app;
 
 // Print handy startup messages
-var token = jwt.sign({}, config.jwtSecret);
-var url = URL.parse(config.webRoot);
+const token = jwt.sign({}, config.jwtSecret);
+let url = URL.parse(config.webRoot);
 url.auth = 'a:' + token;
 url = URL.format(url);
 
