24587493695f49692368770daa09e64b69fa11c0
Jason von Nieda <jason@vonnieda.org>
2020-01-15 23:46:32 -0600

    Added WEB_ROOT to app.json as a temporary fix. Will eventually be automated. Fixed SQL error in setup.js.

--- setup.js
+++ setup.js
@@ -8,9 +8,7 @@ const db = new Pool({
 (async () => {
     try {
         await db.query('create table entries (createdat timestamp with time zone, text text)');
-        await db.query('insert into entries values (now(), "Today I started a new diary.");)');
-        var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-        console.log(fullUrl);
+        await db.query('insert into entries values (now(), "Today I started a new diary.")');
     } catch (e) {
         console.err(e);
     }
