8e34fdeab5945a9eaa1552169cfc0545c59cba10
Jason von Nieda <jason@vonnieda.org>
2020-01-15 23:54:15 -0600

    I guess I've just completely forgotten SQL. Cool.

--- setup.js
+++ setup.js
@@ -7,10 +7,10 @@ const db = new Pool({
 
 (async () => {
     try {
-        await db.query('create table entries (createdat timestamp with time zone, text text)');
-        await db.query('insert into entries values (now(), "Today I started a new diary.")');
+        await db.query("create table entries (createdat timestamp with time zone, text text)");
+        await db.query("insert into entries values (now(), 'Today I started a new diary.')");
     } catch (e) {
-        console.err(e);
+        console.error(e);
     }
 })();
 
